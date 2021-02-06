e2dc20340feba6db45f1076580d0ffff6b0ce970
apolcarv <alejandropolocarvajal@gmail.com>
2021-02-06 14:02:55 -0500

    update file Environment and newman execute.sh

--- newman_execute.sh
+++ newman_execute.sh
@@ -1,1 +1,11 @@
+//http_proxy=""
+collections="https://www.getpostman.com/collections/4c8b7a7ccad36a1f7df7"
+//evironment=""
+//variables_globals=""
+interactionCount=2
+project="Test"
 
+
+newman run $collections -r "cli","htmlextra" -$evironment -$variables_globals -$interactionCount --reporter-htmlextra-export ".reports/reports.html" --reporter-htmlextra-title "Report of web Services $project" --reporter-htmlextra-browserTitle "Report $project"
+
+//set HTTP_PROXY=$http_proxy && newman run $collections -r "cli","htmlextra" -$evironment -$variables_globals -$interactionCount --reporter-htmlextra-export ".reports/reports.html" --reporter-htmlextra-title "Report of web Services $project" --reporter-htmlextra-browserTitle "Report $project"
